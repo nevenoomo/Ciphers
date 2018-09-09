1efc68b1a252beae1f60d7823ba269c367f2b24b
Neve Noomo <d3ad.l1n3@yandex.ru>
2018-09-09 17:39:21 +0300

    Format code

--- Transposition/route.cpp
+++ Transposition/route.cpp
@@ -3,71 +3,71 @@
 vector<int> parseNums(string numbers);
 void validate(vector<int> v);
 
-int main(int argc, char const *argv[])
-{
-    ifstream input(argv[1]);
-    //TODO error check
-    string mode, numbers;
-    input>>mode;
-    input>>numbers;
-    while(input.peek() == '\n' || input.peek() == '\r'){//skip endl
-        input.ignore();
-    }
+int main(int argc, char const *argv[]) {
+  ifstream input(argv[1]);
+  // TODO error check
+  string mode, numbers;
+  input >> mode;
+  input >> numbers;
+  while (input.peek() == '\n' || input.peek() == '\r') {  // skip endl
+    input.ignore();
+  }
 
-    vector<int> nums = parseNums(numbers);
+  vector<int> nums = parseNums(numbers);
 
-    Cipherer c(nums.data(), nums.size(), input);//&nums[0] if earlier then c++11
-    if (mode == "encrypt"){
-        c.encrypt();
-    }else if(mode == "decrypt"){
-        c.decrypt();
-    }else{
-        //TODO error
-    }
-    getchar();
-    return 0;
+  Cipherer c(nums.data(), nums.size(), input);  //&nums[0] if earlier then c++11
+  if (mode == "encrypt") {
+    c.encrypt();
+  } else if (mode == "decrypt") {
+    c.decrypt();
+  } else {
+    // TODO error
+  }
+  getchar();
+  return 0;
 }
 
-vector<int> parseNums(string numbers){
-    stringstream num;
-    vector<int> v;
-    bool comma = numbers[0] == ',';
-    if (comma){
-        //TODO error in num order
-    }
-    if (numbers.size() == 0){
-        //TODO empty string error
-    }
+vector<int> parseNums(string numbers) {
+  stringstream num;
+  vector<int> v;
+  bool comma = numbers[0] == ',';
+  if (comma) {
+    // TODO error in num order
+  }
+  if (numbers.size() == 0) {
+    // TODO empty string error
+  }
 
-    for(size_t i = 0; i < numbers.size(); i++)
-    {
-       if(isdigit(numbers[i])){
-           num << numbers[i];
-           comma = false;
-       }else if(numbers[i] == ','){
-           if (comma) ;//TODO error
-           v.push_back(stoi(num.str()));
-           num.str(string());//reset stream
-           comma = true;
-       }else{
-           //TODO error
-       }
+  for (size_t i = 0; i < numbers.size(); i++) {
+    if (isdigit(numbers[i])) {
+      num << numbers[i];
+      comma = false;
+    } else if (numbers[i] == ',') {
+      if (comma)
+        ;  // TODO error
+      v.push_back(stoi(num.str()));
+      num.str(string());  // reset stream
+      comma = true;
+    } else {
+      // TODO error
     }
-    v.push_back(stoi(num.str()));
-    if (comma) ;//TODO nums end with comma error
-    validate(v);
-    return v;
+  }
+  v.push_back(stoi(num.str()));
+  if (comma)
+    ;  // TODO nums end with comma error
+  validate(v);
+  return v;
 }
 
-void validate(vector<int> v){//TODO check if copies vector correctly
-    sort(v.begin(), v.end());
-    bool valid = true;
+void validate(vector<int> v) {  // TODO check if copies vector correctly
+  sort(v.begin(), v.end());
+  bool valid = true;
 
-    for(int i = 0; i < (int)v.size() && valid; i++){
-        valid = v[i] == i + 1;
-    }
+  for (int i = 0; i < (int)v.size() && valid; i++) {
+    valid = v[i] == i + 1;
+  }
 
-    if(!valid){
-        //TODO not valid num sequence
-    }
+  if (!valid) {
+    // TODO not valid num sequence
+  }
 }
--- Transposition/route.h
+++ Transposition/route.h
@@ -1,122 +1,120 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstring>
-#include <sstream>
-#include <vector>
-#include <cctype>
 #include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-class Cipherer{
-    char ** table;
-    int * key;
-    int rows;
-    int capacity;
-    int columns;
-    ifstream * text;
+class Cipherer {
+  char** table;
+  int* key;
+  int rows;
+  int capacity;
+  int columns;
+  ifstream* text;
 
-    int gettextsize() const{
-        int pos = text->tellg();
-        text->seekg(0, ios_base::end);
-        int textsize = (int)text->tellg() - pos;
-        text->seekg(pos);
+  int gettextsize() const {
+    int pos = text->tellg();
+    text->seekg(0, ios_base::end);
+    int textsize = (int)text->tellg() - pos;
+    text->seekg(pos);
 
-        return textsize;
+    return textsize;
+  }
+
+  void fillEnc() {
+    char buf[columns];
+    int textsize = gettextsize();
+    int times = textsize / columns;
+    if (textsize % columns != 0) times += 1;
+    for (int i = 0; i < times; i++) {
+      memset(buf, ' ', columns * sizeof(char));  // TODO works??
+      text->read(buf, columns);
+      if (rows == capacity) {
+        table = (char**)realloc(
+            table, (capacity += 10) * sizeof(char*));  // TODO check for error
+      }
+      table[rows] = (char*)malloc(columns * sizeof(char));
+      memcpy(table[rows], buf, columns * sizeof(char));
+      rows++;
+    }
+  }
+
+  void fillDec() {
+    int textsize = gettextsize();
+    rows = textsize / columns;  // TODO if (textsize%columns!=0) then text has
+                                // not been ecrypted correctly
+    if (rows > capacity) {
+      table =
+          (char**)realloc(table, rows * sizeof(char*));  // TODO check for error
     }
 
-    void fillEnc(){
-        char buf[columns];
-        int textsize = gettextsize();
-        int times = textsize/columns;
-        if (textsize%columns!=0) times += 1;
-        for(int i = 0; i < times; i++){
-            memset(buf, ' ', columns*sizeof(char));//TODO works??
-            text->read(buf, columns);
-            if (rows == capacity){
-                table = (char**)realloc(table, (capacity+=10)*sizeof(char*));//TODO check for error
-            }
-            table[rows] = (char*)malloc(columns*sizeof(char));
-            memcpy(table[rows], buf, columns*sizeof(char));                  
-            rows++;
-        }
+    for (int i = 0; i < rows; i++) {
+      table[i] = (char*)malloc(sizeof(char) * columns);
     }
 
-    void fillDec(){
-        int textsize = gettextsize();
-        rows = textsize/columns;//TODO if (textsize%columns!=0) then text has not been ecrypted correctly 
-        if (rows > capacity){
-            table = (char**)realloc(table, rows*sizeof(char*));//TODO check for error
-        }
-        
-        for(int i = 0; i < rows; i++)
-        {
-            table[i] = (char*)malloc(sizeof(char)*columns);
-        }
-
-        char buf[rows];
-        for(int i = 0; i < columns; i++)
-        {
-            if(text->bad()) ; //TODO errorhandle 
-            text->read(buf, rows);
-            setcolumn(key[i], buf);
-        }  
+    char buf[rows];
+    for (int i = 0; i < columns; i++) {
+      if (text->bad())
+        ;  // TODO errorhandle
+      text->read(buf, rows);
+      setcolumn(key[i], buf);
     }
-public:
-    Cipherer(int * header, const int ncols, const ifstream& input): rows(0), capacity(10), columns(ncols), text((ifstream *)&input){
-        table = (char**)malloc(sizeof(char*)*10);
-        key = (int*)malloc(sizeof(int)*columns);
-        for(int i = 0; i < columns; i++){
-            key[header[i] - 1] = i;
-        }        
+  }
+
+ public:
+  Cipherer(int* header, const int ncols, const ifstream& input)
+      : rows(0), capacity(10), columns(ncols), text((ifstream*)&input) {
+    table = (char**)malloc(sizeof(char*) * 10);
+    key = (int*)malloc(sizeof(int) * columns);
+    for (int i = 0; i < columns; i++) {
+      key[header[i] - 1] = i;
     }
+  }
 
-    ~Cipherer(){
-        for(int i = 0; i < rows; i++)
-        {
-            free(table[i]);
-        }
-        free(table);
-        free(key);
+  ~Cipherer() {
+    for (int i = 0; i < rows; i++) {
+      free(table[i]);
     }
+    free(table);
+    free(key);
+  }
 
-    void encrypt(){
-        fillEnc();
-        
-        for(int i = 0; i < columns; i++)
-        {
-            cout<<getcolumn(key[i]);
-        }
+  void encrypt() {
+    fillEnc();
+
+    for (int i = 0; i < columns; i++) {
+      cout << getcolumn(key[i]);
     }
+  }
 
-    void decrypt(){
-        fillDec();
+  void decrypt() {
+    fillDec();
 
-        for(int i = 0; i < rows; i++)
-        {
-            for(int j = 0; j < columns ; j++)
-            {
-                cout<<table[i][j];
-            }
-        }
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        cout << table[i][j];
+      }
+    }
+  }
+
+  string getcolumn(int j) {
+    stringstream s;
+
+    for (int i = 0; i < rows; i++) {
+      s << table[i][j];
     }
 
-    string getcolumn(int j){
-        stringstream s;
-        
-        for(int i = 0; i < rows; i++)
-        {
-           s<<table[i][j];
-        }
-        
-        return s.str();
-    }
+    return s.str();
+  }
 
-    void setcolumn(int j, char buf[]){//TODO buf[rows] ok?  
-        for(int i = 0; i < rows; i++)
-        {
-            table[i][j] = buf[i];
-        }     
+  void setcolumn(int j, char buf[]) {  // TODO buf[rows] ok?
+    for (int i = 0; i < rows; i++) {
+      table[i][j] = buf[i];
     }
+  }
 };
