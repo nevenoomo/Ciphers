e202ff9825a7acc4a41fcce73d7f63f3f4aebf77
Neve Noomo <d3ad.l1n3@yandex.ru>
2018-09-15 16:02:32 +0300

    Finish basic features

--- Polibius/polybius.cpp
+++ Polibius/polybius.cpp
@@ -3,56 +3,42 @@
 void envoke_error(const string &message, int code);
 bool validate_params(string &mode, string &lang, int num);
 
-int main(int argc, char const *argv[])
-{
-    if (argc != 2)
-        envoke_error("Filename expected", COMMAND_SYNTAX_ERROR);
-
-    ifstream input(argv[1]);
-    if (!input.is_open())
-        envoke_error("File is not accessible", ACCESS_ERROR);
-
-    string mode, lang_and_number;
-    getline(input, mode);
-    getline(input, lang_and_number);
-    if (!input.good())
-        envoke_error("File structure is incorect", FILE_SYNTAX_ERROR);
-    string lang;
-    int num;
-    stringstream s(lang_and_number);
-    s >> lang >> num;
-    if (!s.good())
-        ; // TODO envoke error
-    validate_params(mode, lang, num);
-    Polybius p(mode, lang, num, input);
-    p.process();
-    getchar();
-    return 0;
+int main(int argc, char const *argv[]) {
+  if (argc != 2) envoke_error("Filename expected", COMMAND_SYNTAX_ERROR);
+  ifstream input(argv[1]);
+  if (!input.is_open()) envoke_error("File is not accessible", ACCESS_ERROR);
+  string mode, lang_and_number;
+  getline(input, mode);
+  getline(input, lang_and_number);
+  if (!input.good()) envoke_error("File structure is incorect", SYNTAX_ERROR);
+  string lang;
+  int num = 0;
+  stringstream s(lang_and_number);
+  s >> lang >> num;
+  if (num == 0 || lang == "")
+    envoke_error("File structure is incorect", SYNTAX_ERROR);
+  validate_params(mode, lang, num);
+  Polybius(mode, lang, num, input).process();
+  getchar();
+  return 0;
 }
 
-void envoke_error(const string &message, int code)
-{
-    cerr << "ERROR: " << message << endl;
-    getchar();
-    exit(code);
+void envoke_error(const string &message, int code) {
+  cerr << "ERROR: " << message << endl;
+  getchar();
+  exit(code);
 }
 
-bool validate_params(string &mode, string &lang, int num)
-{ //num must be odd and only english is supported
-    if (mode != "encrypt" && mode != "decrypt")
-    {
-        envoke_error("Mode is not recognosed", FILE_SYNTAX_ERROR);
-    }
-    else if (lang != "english")
-    {
-        envoke_error("Language is not supported", FILE_SYNTAX_ERROR); //TODO try + string
-    }
-    else if (num % 2 == 0)
-    {
-        envoke_error("Odd number expected", FILE_SYNTAX_ERROR);
-    }
-    else if (num < 0){
-        envoke_error("Positive number expected", FILE_SYNTAX_ERROR);
-    }
-    return true;
+bool validate_params(string &mode, string &lang, int num) {
+  // num must be odd and only english is supported
+  if (mode != "encrypt" && mode != "decrypt") {
+    envoke_error("Mode is not recognosed", SYNTAX_ERROR);
+  } else if (lang != "english") {
+    envoke_error("Language is not supported", SYNTAX_ERROR);
+  } else if (num % 2 == 0) {
+    envoke_error("Odd number expected", SYNTAX_ERROR);
+  } else if (num < 0) {
+    envoke_error("Positive number expected", SYNTAX_ERROR);
+  }
+  return true;
 }
--- Polibius/polybius.h
+++ Polibius/polybius.h
@@ -1,51 +1,98 @@
 #include <algorithm>
+#include <cctype>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <cctype>
 
 using namespace std;
 
 #define COMMAND_SYNTAX_ERROR 1
 #define ACCESS_ERROR 2
-#define FILE_SYNTAX_ERROR 3
+#define SYNTAX_ERROR 3
 #define UNVALID_TEXT 4
+#define ENG_DIM 5
 
 void envoke_error(const string &message, int code);
 
-class Polybius
-{
-    bool enc;//true => encrypt, false => decrypt
-    enum lang
-    {
-        ENGLISH
-    } lang;
-    ifstream & text;
-    int rotation;
-    vector<int> cipher;
+class Polybius {
+  bool enc;  // true => encrypt, false => decrypt
+  enum lang { ENGLISH } lang;
+  ifstream &text;
+  int rotation;
+  vector<int> cipher;
 
-    void fill(){
-        int c = ' ', coordinate = 0, offset = 0;
-        while((c = toupper(text.get()))!= EOF){
-            if (!isalpha(c)) envoke_error("Text is not valid", UNVALID_TEXT);
-            if (c == 'Z') c = 'Y';
-            offset = c - 'A';
-            coordinate = (offset/5+1)*10 + (offset%5+1);
-            cipher.push_back(coordinate);
-        }
+  void fill() {
+    int c = ' ', coordinate = 0, offset = 0;
+    while ((c = text.get()) != EOF) {
+      if (!isalpha(c)) envoke_error("Text is not valid", UNVALID_TEXT);
+      c = toupper(c);
+      if (c == 'J') c = 'I';
+      offset = c - 'A' + ((c > 'I') ? (-1) : 0);
+      coordinate = (offset / ENG_DIM + 1) * 10 + (offset % ENG_DIM + 1);
+      cipher.push_back(coordinate);
     }
-public:
-    Polybius(string& mode, string& lang, int num, ifstream& input): text(input), rotation(num){
-        if (lang == "english"){
-            this->lang = ENGLISH;
-        }
-        this->enc = mode == "encrypt";
-    }
+  }
 
-    void process(){
-        fill();
-        
+  void rotate() {
+    if (cipher.empty()) return;
+    rotation %= (cipher.size() * 2);
+    rotation /= 2;
+    if (enc) {
+      if (rotation != 0)
+        std::rotate(cipher.begin(), cipher.begin() + rotation, cipher.end());
+      rot1l();
+    } else {
+      if (rotation != 0)
+        std::rotate(cipher.rbegin(), cipher.rbegin() + rotation, cipher.rend());
+      rot1r();
     }
+  }
+
+  void rot1r() {
+    int save = cipher.back() % 10, newcoord = 0;
+
+    for (size_t i = 0; i < cipher.size(); i++) {
+      newcoord = save * 10 + cipher[i] / 10;
+      save = cipher[i] % 10;
+      cipher[i] = newcoord;
+    }
+  }
+
+  void rot1l() {
+    int save = cipher.front() / 10, newcoord = 0;
+
+    for (int i = cipher.size() - 1; i >= 0; i--) {
+      newcoord = (cipher[i] % 10) * 10 + save;
+      save = cipher[i] / 10;
+      cipher[i] = newcoord;
+    }
+  }
+
+  void print() {
+    int offset = 0;
+    char c = 0;
+    for (size_t i = 0; i < cipher.size(); ++i) {
+      offset = (cipher[i] / 10 - 1) * ENG_DIM + (cipher[i] % 10 - 1);
+      c = 'A' + offset;
+      if (c > 'I') c++;
+      cout << c;
+    }
+  }
+
+ public:
+  Polybius(string &mode, string &language, int num, ifstream &input)
+      : text(input), rotation(num) {
+    if (language == "english") {
+      lang = ENGLISH;
+    }
+    enc = mode == "encrypt";
+  }
+
+  void process() {
+    fill();
+    rotate();
+    print();
+  }
 };
