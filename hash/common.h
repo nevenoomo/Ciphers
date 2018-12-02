ade215c8d0f4f9a8626829d4fdc843618f41531f
Neve Noomo <d3ad.l1n3@yandex.ru>
2018-12-03 01:12:25 +0300

    Add basic GOST functions

---
+++ hash/common.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdio>
+#include <iostream>
+#include <cstdint>
+#include <cstring>
+
+#define BLOCK_SIZE 64
+typedef uint8_t V512[BLOCK_SIZE];
+
+using namespace std;
+
---
+++ hash/hash.h
@@ -0,0 +1,122 @@
+#pragma once
+#include "common.h"
+#include "hash_data.h"
+
+class Streebog {
+    V512 h, N, Sigma;
+    FILE *in;
+    bool is_512;
+    size_t f_size, len;
+
+    void add_in_ring(V512 a, V512 b, V512 dest) {
+        uint16_t overrun = 0;
+
+        for (size_t i = BLOCK_SIZE - 1; i >= 0; i--) {
+            overrun = a[i] + b[i] + (overrun >> 8);
+            dest[i] = (uint8_t)overrun;
+        }
+    }
+
+    void add_in_ring(V512 a, uint32_t b, V512 dest) {
+        for (size_t i = BLOCK_SIZE - 1; i >= 0; i--) {
+            b += (uint32_t)a[i];
+            dest[i] = b;
+            b >>= 8;
+        }
+    }
+
+    void xor512(V512 a, V512 b, V512 dest) {
+        for (size_t i = BLOCK_SIZE - 1; i >= 0; i--) {
+            dest[i] = a[i] ^ b[i];
+        }
+    }
+
+    void X(V512 k, V512 a, V512 dest) { xor512(k, a, dest); }
+
+    void S(V512 a) {
+        for (size_t i = BLOCK_SIZE - 1; i >= 0; i--)
+            a[i] = PI[a[i]];
+    }
+
+    void P(V512 a) {
+        V512 tmp;
+        for (size_t i = BLOCK_SIZE - 1; i >= 0; i--)
+            tmp[i] = a[TAU[i]];
+        memcpy(a, tmp, BLOCK_SIZE);
+    }
+
+    void L(V512 a) {
+        uint64_t *tmp1 = (uint64_t *)a;
+        uint64_t tmp2[8];
+        memset(tmp2, 0x00, BLOCK_SIZE);
+        for (size_t i = 7; i >= 0; i--) {
+            for (size_t j = BLOCK_SIZE - 1; j >= 0; j--) {
+                if ((tmp1[i] >> j) & 1 == 1) { // check the val of bit, then xor
+                    tmp2[i] ^= A[BLOCK_SIZE - 1 - j];
+                }
+            }
+        }
+        memcpy(a, tmp2, BLOCK_SIZE);
+    }
+
+    void get_key(V512 K, int i) {
+        xor512(K, C[i - 1], K);
+        S(K);
+        P(K);
+        L(K);
+    }
+
+    void E(V512 K, V512 m, V512 dest) {
+        X(K, m, dest);
+        for (size_t i = 1; i < 13; i++) {
+            S(dest);
+            P(dest);
+            L(dest);
+            get_key(K, i);
+            X(K, dest, dest);
+        }
+    }
+
+    void g(V512 N, V512 h, V512 m) { // return V512 in h
+        V512 K, tmp;
+        // LPS(h ^ N)
+        xor512(h, N, K);
+        S(K);
+        P(K);
+        L(K);
+
+        // E(LPS(h ^ N), m)
+        E(K, m, tmp);
+
+        // E(LPS(h ^ N), m) ^ h ^ m
+        xor512(tmp, h, tmp);
+        xor512(tmp, m, h);
+    }
+
+    size_t file_size(FILE *f) {
+        size_t size = 0;
+        fseek(f, 0, SEEK_END);
+        size = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        return size;
+    }
+
+    void Part2(V512 m) {
+        g(N, h, m);
+        add_in_ring(N, 512, N);
+        add_in_ring(Sigma, m, Sigma);
+    }
+
+  public:
+    Streebog(FILE *in, bool is_512 = true) : in(in), is_512(is_512) {
+        memset(N, 0x0, BLOCK_SIZE);
+        memset(Sigma, 0x0, BLOCK_SIZE);
+        if (is_512) {
+            memcpy(h, IV512, BLOCK_SIZE);
+        } else {
+            memcpy(h, IV256, BLOCK_SIZE);
+        }
+
+        len = f_size = file_size(in);
+    }
+};
