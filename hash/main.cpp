cbbdebef2b4a61d9a9d8ca41f8505732df8a1536
Neve Noomo <d3ad.l1n3@yandex.ru>
2018-12-04 00:35:18 +0300

    Err2

--- hash/main.cpp
+++ hash/main.cpp
@@ -4,7 +4,7 @@
 int main(){
     FILE * f = fopen("test.txt", "rb"), *out = fopen("test2.txt", "wb");
     Streebog s(f);
-    uint8_t * ret = s.H();//err
+    uint8_t * ret = s.H();
     fwrite(ret, 1, BLOCK_SIZE, out);
     return 0;
 }
